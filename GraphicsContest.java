da6810c87e8e6e0c7a0d5085b9e20abdd97d02c3
S <s>
2015-11-21 07:03:36 -0800

    {"type":"TI", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -100,6 +100,7 @@ public class GraphicsContest extends GraphicsProgram {
 			pause(DELAY);
 			if (circle.getX()>getWidth() || circle.getY()>getHeight()) {
 				remove(circle);
+				circle=drawCircle();
 			}		
 		}
 	}
