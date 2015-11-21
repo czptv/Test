30a1a230d7c82acc8c926b395c74830a8da3986d
S <s>
2015-11-21 06:17:52 -0800

    {"type":"TI", "runs":"2"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -7,12 +7,16 @@ import acm.program.*;
 import acm.graphics.*;
 import acm.util.*;
 
+import java.awt.event.*;
+
 public class GraphicsContest extends GraphicsProgram {
 
 	/**
 	 * constants
 	 */
 	private static final int DELAY=100;
+	private static final double NEW_GAME_WIDTH=0;
+	private static final double NEW_GAME_ASCENT=0;
 	
 	/**
 	 * instant variables
@@ -31,26 +35,48 @@ public class GraphicsContest extends GraphicsProgram {
 		add(newGame,(getWidth()-newGame.getWidth())/2.0,getHeight()-2*newGame.getAscent());
 		
 		//special effects
-		createEffect();
-		/*
+		createEffect(newGame);
+		
 		//start game
 		addMouseListeners();
 		removeAll();
-		playGame();*/
+		playGame();
 	}
 
 	/**
 	 * make certain image move randomly on the screen
 	 */
-	private void createEffect() {
-		//draw circle
+	private void createEffect(GLabel newGame) {
+		//draw circles on the screen
+		GImage c1=drawCircle();
+		GImage c2=drawCircle();
+		GImage c3=drawCircle();
+		GImage c4=drawCircle();
+		GImage c5=drawCircle();
+		//get velocity and move
+		while() {
+			
+		}
+	}
+	
+	/**
+	 * draw one circle on the screen
+	 * @return circle
+	 */
+	private GImage drawCircle() {
 		GImage circle=new GImage("circle.png");
 		double ratio=rgen.nextDouble();
 		circle.scale(ratio,ratio);
 		int x=rgen.nextInt(getWidth());
 		int y=rgen.nextInt(getHeight());
 		add(circle,x,y);
-		//get velocity and move
+		return circle;
+	}
+	/**
+	 * get velocity and move
+	 * @param circle
+	 */
+	private void moveCircle(GImage circle) {
 		while(!(circle.getX()>getWidth() && circle.getY()>getHeight())) {
 			int vx=rgen.nextInt(2,6);
 			int vy=rgen.nextInt(2,6);
@@ -60,4 +86,16 @@ public class GraphicsContest extends GraphicsProgram {
 		remove(circle);
 	}
 	
+	
+	private boolean mouseClicked(MouseEvent e) {
+		double x=(getWidth()-NEW_GAME_WIDTH)/2.0;
+		double y=getHeight()-2*NEW_GAME_ASCENT;
+		boolean clicked=false;
+		boolean xContain=e.getX()>x && e.getX()<x+NEW_GAME_WIDTH;
+		boolean yContain=e.getY()>y && e.getY()<y+NEW_GAME_ASCENT;
+		if()) {
+			clicked=true;
+		}
+		return clicked;
+	}
 }
